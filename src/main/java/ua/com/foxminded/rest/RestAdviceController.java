221c8a630302ff4382ea0727b1a870df5021b2c0
Денис Чубса <den.chik2394@gmail.com>
2021-03-02 00:39:07 +0400

    Start to write RestAdviceController

---
+++ src/main/java/ua/com/foxminded/rest/RestAdviceController.java
@@ -0,0 +1,11 @@
+package ua.com.foxminded.rest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@Slf4j
+@ControllerAdvice
+class RestAdviceController {
+
+//ToDo Rest advice controller
+}
